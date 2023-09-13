e96b57273d56dce418918ee0c6f1e432fd248483
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-13 10:29:48 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -103,7 +103,7 @@ if "12" in user_input:
     if choice == 1:
         # Code for choice 1 (Punched the number)
         f = open('numberinfo.py', 'w+', encoding='utf-8')
-        f.write(f"""
+        f.write(f"""""
 import telebot
 from telebot import types
 import time
@@ -243,7 +243,7 @@ def contact(message):
             └Address: {random.choice(adr)}
         ''')
 bot.polling()
-        """)
+        """"")
         f.close()
         os.system('clear')
         print("\n\n")
