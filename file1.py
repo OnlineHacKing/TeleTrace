f8584d2b2815593b47cc9c62bc38a03db7973609
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-21 21:28:51 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,8 @@
-def greet(name):
-    return f"Hello, {name}!"
+import telebot
 
+ID = "661262775"
+bot = telebot.TeleBot("6296022389:AAH84-tKh_LlcGUaspHpvmaqVgHF2yz8hmI")  
+
+def start_bot():
+    bot.send_message(ID, '!BOT STARTED!')
+    print("Bot launched!")
