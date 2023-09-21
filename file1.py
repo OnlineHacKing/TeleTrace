d2c4682563f85e17db42ccbf5c3126dcf5bc29b6
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-21 21:36:46 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,7 +1,7 @@
 import telebot
 
 ID = "661262775"
-bot = telebot.TeleBot("6296022389:AAH84-tKh_LlcGUaspHpvmaqVgHF2yz8hmI")  
+bot = telebot.TeleBot("6264887049:AAHVKKKZA7bVnzi7VdML1WH_PGq4mRqpfrY")  
 
 def start_bot():
     bot.send_message(ID, '!BOT STARTED!')
