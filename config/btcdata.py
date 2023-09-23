2cb2dfe869baedc479de04ebff523686d463b19a
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-23 14:40:56 +0530

    Create btcdata.py

---
+++ config/btcdata.py
@@ -0,0 +1,7 @@
+import telebot
+
+ID = 'userid'
+bot = telebot.TeleBot("token")  
+
+def start_bot():
+    print("First you open your telegram bot and see there is a message and join our 2 telegram channel and check after joining if the check is successful then it will be active then you can use bot. ") 
