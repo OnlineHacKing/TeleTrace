0c662a26e061329a2cc2b43da504e442d30483f7
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-09-23 18:49:14 +0530

    Delete bot1.py

--- bot1.py
+++
@@ -1,7 +0,0 @@
-import telebot
-
-ID = "661262775"
-bot = telebot.TeleBot("6264887049:AAHVKKKZA7bVnzi7VdML1WH_PGq4mRqpfrY")  
-
-def start_bot():
-    print("\n\n\033[91m[\033[92mNO ACTIVE\033[91m] \033[0mFirst you open your telegram bot and see there is a message and join our 2 telegram channel and check after joining if the check is successful then it will be active then you can use bot\n\n")
